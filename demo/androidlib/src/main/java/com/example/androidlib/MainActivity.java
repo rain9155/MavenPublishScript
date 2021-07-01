e78b4a97c1a69132fe302158a2f28db5b49f9b0f
chenjianyu <chenjianyu.rain@bytedance.com>
2021-07-01 18:04:28 +0800

    发布脚本增加发布jar支持和发布aar时支持variant

---
+++ demo/androidlib/src/main/java/com/example/androidlib/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+/**
+ * @author chenjianyu
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
