ba5612193bc217c632796610d43ded2ab345f8c0
rain9155 <jianyu9155@gmail.com>
2021-07-11 03:41:15 +0800

    重构发布脚本，AGP3.6以后使用自带的components发布

---
+++ demo/androidlib/src/oversea/java/com/example/androidlib/OverseaActivity.java
@@ -0,0 +1,17 @@
+package com.example.androidlib;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * oversea活动
+ */
+public class OverseaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oversea);
+    }
+}
